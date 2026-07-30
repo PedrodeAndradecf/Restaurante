31321b3d164cd6517f555df1c751db4db6818546
PedrodeAndradecf <pedro.cfandrade@gmail.com>
2026-07-30 16:01:35 -0300

    feat: classe pagamento, enum statusPagamento

--- src/main/java/Backend/Restaurante/domain/Pagamento.java
+++ src/main/java/Backend/Restaurante/domain/Pagamento.java
@@ -1,6 +1,11 @@
 package Backend.Restaurante.domain;
 
+import Backend.Restaurante.domain.enums.FormaPagamento;
+import Backend.Restaurante.domain.enums.StatusPagamento;
 import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "pagamentos")
@@ -10,5 +15,83 @@ public class Pagamento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    
+    private BigDecimal valor;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "forma_pagamento")
+    private FormaPagamento formaPagamento;
+
+    @Enumerated(EnumType.STRING)
+    private StatusPagamento statusPagamento;
+
+    private String codigoTransacaoExterna;
+
+    private LocalDateTime criadoEm;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+
+    @PrePersist
+    public void prePersist(){
+        this.criadoEm = LocalDateTime.now();
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public StatusPagamento getStatusPagamento() {
+        return statusPagamento;
+    }
+
+    public void setStatusPagamento(StatusPagamento statusPagamento) {
+        this.statusPagamento = statusPagamento;
+    }
+
+    public FormaPagamento getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(FormaPagamento formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public String getCodigoTransacaoExterna() {
+        return codigoTransacaoExterna;
+    }
+
+    public void setCodigoTransacaoExterna(String codigoTransacaoExterna) {
+        this.codigoTransacaoExterna = codigoTransacaoExterna;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(LocalDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
 }
---
+++ src/main/java/Backend/Restaurante/domain/enums/StatusPagamento.java
@@ -0,0 +1,8 @@
+package Backend.Restaurante.domain.enums;
+
+public enum StatusPagamento {
+    PENDENTE,
+    APROVADO,
+    RECUSADO,
+    CANCELADO
+}
